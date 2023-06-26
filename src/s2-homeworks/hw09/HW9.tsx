4c3310c850d8ac1cb3503060dce3ce4868084bbd
7lad48 <2335213@gmail.com>
2023-06-26 17:56:34 +0300

    delete text letter at DOM structure

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -13,7 +13,6 @@ const HW9 = () => {
     return (
         <div id={'hw9'}>
             <div className={s2.hwTitle}>Homework #9</div>
-g
             <div className={s2.hw}>
                 <Clock />
             </div>
